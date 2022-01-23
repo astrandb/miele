f3902187dbdacab04b25f980f281d50091d206ef
Åke Strandberg <ake@strandberg.eu>
2022-01-23 13:57:36 +0100

    Cleanup

--- custom_components/miele/diagnostics.py
+++ custom_components/miele/diagnostics.py
@@ -1,15 +1,12 @@
 """Diagnostics support for Miele."""
 from __future__ import annotations
 
-# from dataclasses import asdict
-
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from . import get_coordinator
 from .const import DOMAIN
 
 TO_REDACT = {CONF_PASSWORD, CONF_USERNAME, "access_token", "refresh_token"}
@@ -19,7 +16,9 @@ async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, config_entry: ConfigEntry
 ) -> dict:
     """Return diagnostics for a config entry."""
-    coordinator: DataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+    coordinator: DataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id][
+        "coordinator"
+    ]
 
     diagnostics_data = {
         "info": async_redact_data(config_entry.data, TO_REDACT),
