054dd3478870155aa4e7b0eea899684544e50b6b
Åke Strandberg <ake@strandberg.eu>
2022-02-12 09:37:37 +0100

    Bump version

--- custom_components/miele/const.py
+++ custom_components/miele/const.py
@@ -1,7 +1,7 @@
 """Constants for the Miele integration."""
 
 DOMAIN = "miele"
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 
 MIELE_API = "https://api.mcs3.miele.com/v1"
 OAUTH2_AUTHORIZE = "https://api.mcs3.miele.com/thirdparty/login"
