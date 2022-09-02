2c575fc389ad367d829985c9967b3ea7e9178d79
Åke Strandberg <ake@strandberg.eu>
2022-09-02 14:35:05 +0200

    Debug log for abs time sensors

--- custom_components/miele/sensor.py
+++ custom_components/miele/sensor.py
@@ -706,6 +706,7 @@ class MieleSensor(CoordinatorEntity, SensorEntity):
             )
             if mins == 0:
                 return None
+            _LOGGER.debug("Key:  %s | Mins: %s | Now: %s | State: %s", self.entity_description.key, mins, now, (now + timedelta(minutes=mins)).strftime("%H:%M"))
             return (now + timedelta(minutes=mins)).strftime("%H:%M")
 
         if self.entity_description.key in [
