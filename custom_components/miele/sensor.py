43c1560f4d7f15835730065c88124716ecaf4a86
Åke Strandberg <ake@strandberg.eu>
2022-09-19 12:45:30 +0200

    More detailed logging of absolute timestamps

--- custom_components/miele/sensor.py
+++ custom_components/miele/sensor.py
@@ -706,7 +706,7 @@ class MieleSensor(CoordinatorEntity, SensorEntity):
             )
             if mins == 0:
                 return None
-            _LOGGER.debug("Key:  %s | Mins: %s | Now: %s | State: %s", self.entity_description.key, mins, now, (now + timedelta(minutes=mins)).strftime("%H:%M"))
+            _LOGGER.debug("Key:  %s | Dev: %s | Mins: %s | Now: %s | State: %s", self.entity_description.key, self._ent, mins, now, (now + timedelta(minutes=mins)).strftime("%H:%M"))
             return (now + timedelta(minutes=mins)).strftime("%H:%M")
 
         if self.entity_description.key in [
@@ -719,6 +719,7 @@ class MieleSensor(CoordinatorEntity, SensorEntity):
             )
             if mins == 0:
                 return None
+            _LOGGER.debug("Key:  %s | Dev: %s | Mins: %s | Now: %s | State: %s", self.entity_description.key, self._ent, mins, now, (now + timedelta(minutes=mins)).strftime("%H:%M"))
             return (now - timedelta(minutes=mins)).strftime("%H:%M")
 
         # Log raw and localized values for programID etc
