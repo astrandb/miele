2fb750375962a8e2668e84b1ee9e283510f3c244
Andrea Turri <andrea.turri92@gmail.com>
2022-12-24 11:34:29 +0100

    [tumble-dryer] add drying step sensor

--- custom_components/miele/const.py
+++ custom_components/miele/const.py
@@ -280,6 +280,16 @@ STATE_PROGRAM_ID = {
     ROBOT_VACUUM_CLEANER: ROBOT_VACUUM_CLEANER_PROGRAM_ID,
 }
 
+STATE_DRYING_STEP = {
+    0: "extra_dry",
+    1: "normal_plus",
+    2: "normal",
+    3: "slightly_dry",
+    4: "hand_iron_1",
+    5: "hand_iron_2",
+    6: "machine_iron",
+}
+
 COLORS = "colors"
 DEVICE_NAME = "deviceName"
 LIGHT = "light"
