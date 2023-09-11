f807a153ba217d79f6555e7e52eb707697731645
Åke Strandberg <ake@strandberg.eu>
2023-09-11 15:08:30 +0000

    Add zeroconf discovery

--- custom_components/miele/config_flow.py
+++ custom_components/miele/config_flow.py
@@ -7,12 +7,15 @@ from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.components import persistent_notification
+from homeassistant.components import persistent_notification, zeroconf
+from homeassistant.const import CONF_HOST, CONF_MAC, CONF_NAME, CONF_PORT
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
+from homeassistant.helpers.device_registry import format_mac
 
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
 
 class OAuth2FlowHandler(
     config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
@@ -22,6 +25,7 @@ class OAuth2FlowHandler(
     DOMAIN = DOMAIN
 
     entry = None
+    name = None
 
     @property
     def logger(self) -> logging.Logger:
@@ -74,3 +78,35 @@ class OAuth2FlowHandler(
             await self.hass.config_entries.async_reload(existing_entry.entry_id)
             return self.async_abort(reason="reauth_successful")
         return await super().async_oauth_create_entry(data)
+
+    async def async_step_zeroconf(
+        self, discovery_info: zeroconf.ZeroconfServiceInfo
+        ) -> FlowResult:
+        """Prepare configuration for a Zeroconf discovered Miele device."""
+        self.name = discovery_info.name.split(".", 1)[0]
+        return await self.async_step_zeroconf_confirm(
+            {
+                CONF_HOST: discovery_info.host,
+                CONF_NAME: self.name,
+                CONF_PORT: discovery_info.port,
+            }
+        )
+
+    async def async_step_zeroconf_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle a flow initiated by zeroconf."""
+
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+        if user_input is not None:
+            try:
+                return await self.async_step_user()
+            except:  # (CannotConnect, ConnectionClosed):
+                # Device became network unreachable after discovery.
+                # Abort and let discovery find it again later.
+                return self.async_abort(reason="cannot_connect")
+        return self.async_show_form(
+            step_id="zeroconf_confirm",
+            description_placeholders={CONF_NAME: self.name},
+        )
