30c4c3f6694da1f346ab4871dff1a9d34ed28c4f
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-08-06 14:45:07 +0200

    Update number.py

--- custom_components/miele/number.py
+++ custom_components/miele/number.py
@@ -162,7 +162,7 @@ async def async_setup_entry(
                     data_tag=f"state|plateStep|{plate_no}|value_raw",
                     icon="mdi:stove",
                     translation_key="plate",
-                    translation_placeholders={"plate_no": f"{plate_no+1}"}
+                    translation_placeholders={"plate_no": f"{plate_no+1}"},
                     zone=plate_no,
                     native_min_value=0.0,
                     native_max_value=10.0,
